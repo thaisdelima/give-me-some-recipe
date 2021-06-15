d43fe9ae93ab0a0a82122ebc91d1c21b616dcbb0
Thaís de Lima <thaislimaf@gmail.com>
2021-06-14 21:59:47 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # give-me-some-recipe
+
+api used in this project https://www.themealdb.com/api.php
---
+++ give-me-some-recipe/src/pages/App.js
@@ -0,0 +1,16 @@
+// import logo from "../logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p></p>
+        <button className="btn-gmsr">Give me some recipe</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
