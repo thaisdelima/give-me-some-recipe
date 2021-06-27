aed6ce7b5de39ac5d24285416f8e6ce1d0ce1b65
Thaís de Lima <thaislimaf@gmail.com>
2021-06-27 19:46:33 -0300

    dinamicaly load one recipe

--- give-me-some-recipe/src/pages/RecipePage.js
+++ give-me-some-recipe/src/pages/RecipePage.js
@@ -1,45 +1,88 @@
-import React, { Component } from "react";
+import React from "react";
+import "./RecipePage.css";
 // import PropTypes from "prop-types";
-import { Container, Box, Text, Flex, Spacer } from "@chakra-ui/react";
+import { Container, Box, Text, Flex } from "@chakra-ui/react";
 import IngredientsList from "../components/IngredientsList";
+import axios from "axios";
 
-export default class RecipePage extends Component {
-  // static propTypes = {
-  //   prop: PropTypes,
-  // };
+export default class RecipePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipe: "",
+      title: "",
+      ingredients: "",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/random.php`)
+      .then((res) => {
+        const resp = res.data.meals[0];
+        this.setState({ recipe: resp.strInstructions });
+        this.setState({ title: resp.strMeal });
+        this.setState({ ingredients: resp.strIngredient1 });
+        console.log("resp", resp);
+      });
+  }
 
   render() {
-    return (
-      <div>
-        <Text fontSize="6xl" color="black">
-          Recipe Name
+    const ingredients = this.state.ingredients;
+    return ingredients ? (
+      <div className="RecipePage">
+        <Text fontSize="6xl" m={6} color="black">
+          {this.state.title}
         </Text>
-        <Flex>
-          <Box m={5}>
-            <IngredientsList />
+        <Flex wrap justify="center">
+          <Box m={2}>
+            <IngredientsList dataParentToChild={ingredients} />
           </Box>
-          <Spacer />
           <Box
-            maxW="md"
+            w="50%"
             borderWidth="1px"
             borderRadius="lg"
             bg="white"
             overflow="hidden"
-            p={10}
-            m={5}>
-            <Text fontSize="4xl" color="black">
+            px={10}
+            py={5}
+            m={2}>
+            <Text fontSize="4xl" color="black" mb={4}>
               Recipe
             </Text>
-            <Container color="black" p={5}>
-              There are many benefits to a joint design and development system.
-              Not only does it bring benefits to the design team, but it also
-              brings benefits to engineering teams. It makes sure that our
-              experiences have a consistent look and feel, not just in our
-              design specs, but in production
-            </Container>
+            <Container color="black">{this.state.recipe}</Container>
           </Box>
         </Flex>
       </div>
+    ) : (
+      <span>Loading recipe...</span>
     );
+    // return (
+    //   <div className="RecipePage">
+    //     <Text fontSize="6xl" m={6} color="black">
+    //       {this.state.title}
+    //     </Text>
+    //     <Flex wrap justify="center">
+    //       <Box m={2}>
+    //         <IngredientsList dataParentToChild={ingredients} />
+    //       </Box>
+    //       <Box
+    //         w="50%"
+    //         borderWidth="1px"
+    //         borderRadius="lg"
+    //         bg="white"
+    //         overflow="hidden"
+    //         px={10}
+    //         py={5}
+    //         m={2}>
+    //         <Text fontSize="4xl" color="black" mb={4}>
+    //           Recipe
+    //         </Text>
+    //         <Container color="black">{this.state.recipe}</Container>
+    //       </Box>
+    //     </Flex>
+    //   </div>
+    // );
   }
 }
