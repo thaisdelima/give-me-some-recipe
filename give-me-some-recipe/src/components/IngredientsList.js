d0ca44d82c7b679933d4548b48528475f2d050c1
Thaís de Lima <thaislimaf@gmail.com>
2021-06-27 19:45:06 -0300

    reactively render one ingredient

--- give-me-some-recipe/src/components/IngredientsList.js
+++ give-me-some-recipe/src/components/IngredientsList.js
@@ -1,24 +1,32 @@
-import React, { Component } from "react";
-import { Container, Checkbox, Box, Text, SimpleGrid } from "@chakra-ui/react";
+import React from "react";
+import { Checkbox, Box, Text, SimpleGrid } from "@chakra-ui/react";
 
-export default class IngredientsList extends Component {
+export default class IngredientsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: this.props.dataParentToChild,
+    };
+  }
+
   render() {
+    const data = this.state.ingredients;
     return (
       <div>
-        <Box
-          borderWidth="1px"
-          borderRadius="lg"
-          bg="white"
-          overflow="hidden"
-          p={[0, 10]}>
-          <Text fontSize="4xl" color="black">
+        <Box borderWidth="1px" borderRadius="lg" bg="white" px={10} py={5}>
+          <Text fontSize="4xl" color="black" mb={4}>
             Ingredients
           </Text>
-          <SimpleGrid color="black" columns={1} spacing={10} p={5}>
-            <Checkbox defaultIsChecked>Checkbox</Checkbox>
-            <Checkbox defaultIsChecked>Checkbox</Checkbox>
-            <Checkbox defaultIsChecked>Checkbox</Checkbox>
-            <Checkbox defaultIsChecked>Checkbox</Checkbox>
+          <SimpleGrid
+            justifyContent="start"
+            color="black"
+            columns={1}
+            spacing={10}
+            minChildWidth="220px"
+            p={3}>
+            <Checkbox size="lg" colorScheme="orange">
+              {data}
+            </Checkbox>
           </SimpleGrid>
         </Box>
       </div>
